4d15f56247d06365adaa809123ec1d4ae3e7ce03
Manav Misra <manavm@visionwebsoft.com>
2023-02-12 15:57:38 -0600

    Add CSS styles for header
    Includes base CSS stuff.

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,13 +1,11 @@
 import PropTypes from "prop-types";
-import Nav from "./nav";
+import Nav from "./nav/nav";
 
 export default function Header({ txt }) {
   return (
-    <header>
-      <h1 className="my-0 bg-gradient-to-r from-cyan-500 to-blue-500 py-8 text-center text-white">
-        {txt}
-      </h1>
-      <Nav />
+    <header className="my-0 bg-gradient-to-r from-cyan-500 to-blue-500 pt-8  text-white">
+      <h1 className="text-center">{txt}</h1>
+      <Nav className="text-right" />
     </header>
   );
 }
