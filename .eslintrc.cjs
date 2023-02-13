ddc7dccb93f8c7c52e9127107ae4cdede0fca46d
Manav Misra <manavm@visionwebsoft.com>
2023-02-12 22:35:39 -0600

    chore: :wrench: allow '_' to be unused

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,7 +18,10 @@ module.exports = {
     sourceType: "module",
   },
   plugins: ["react", "use-encapsulation", "testing-library", "jest-dom"],
-  rules: { "use-encapsulation/prefer-custom-hooks": 1 },
+  rules: {
+    "no-unused-vars": ["error", { varsIgnorePattern: "_" }],
+    "use-encapsulation/prefer-custom-hooks": 1,
+  },
   settings: {
     react: {
       version: "detect",
